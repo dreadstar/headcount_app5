c56462d384d224bdcf3d1a8567b5478a41ed20a4
tyrone thomas <tmt@breakthroughtek.com>
2014-08-28 03:13:11 -0400

    room counters working

--- app/models/door.rb
+++ app/models/door.rb
@@ -1,11 +1,14 @@
 class Door < ActiveRecord::Base
 	belongs_to :location
 	has_many :doormsgs,  foreign_key: :sensor_id, primary_key: :sensor_id
-
+	belongs_to :rooms_to,  foreign_key: :flow_to, class_name: 'Room'
+	belongs_to :rooms_from,  foreign_key: :flow_from, class_name: 'Room'
 	after_commit :update_state
 
 	def update_state
 		self.location.update_state
+		self.rooms_to.update_state
+		self.rooms_from.update_state
 		
 		logger.info "door update_counter"
 	end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,23 @@
 class Room < ActiveRecord::Base
 	belongs_to :location
+	has_many :doors_out, foreign_key: :flow_from, class_name: 'Door'
+	has_many :doors_in,  foreign_key: :flow_to, class_name: 'Door'
+
+	def update_state
+		# sum of all doo
+		new_current_state=0
+		self.doors_in.limit(100)
+		self.doors_out.limit(100)
+		self.doors_in.find_each do |door|
+			new_current_state=new_current_state +door.current_state	
+			logger.info "room doors_in : #{door.current_state}"
+		end
+		self.doors_out.find_each do |door|
+			new_current_state=new_current_state - door.current_state	
+			logger.info "room doors_out : #{door.current_state}"
+		end
+		self.update_attributes(:current_state => new_current_state)
+		logger.debug "room hash: #{self.inspect}"
+		logger.info "room update_counter"
+	end
 end
