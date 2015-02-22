cffe643d6703c80b999ba35b92584ab6d9a579b9
tyrone thomas <tmt@breakthroughtek.com>
2015-02-22 16:38:03 -0500

    tweaking reds connection code

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,7 @@
 # location = ENV["REDISCLOUD_URL"] || 'redis://127.0.0.1:6379/0'
 # location = ENV["REDISCLOUD_URL"] || 'redis://192.168.1.2:6379/0'
-if Rails.env.development?
-  location = ENV["REDISCLOUD_URL"] || 'redis://192.168.1.2:6379/0'
-end
-if Rails.env.production?
-  location = ENV["REDISCLOUD_URL"] || 'redis://redistogo:3f3aed940e56401dee404f708897bf6b@angelfish.redistogo.com:9328/'
-end
+location = ENV["REDISCLOUD_URL"] || 'redis://192.168.1.2:6379/0'
+
 # powerful-savannah-9626.herokuapp.com
 uri = URI.parse(location)
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
