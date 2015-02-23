a1b3228c9fed3cc7e914dc20250858edf782bd5d
tyrone thomas <tmt@breakthroughtek.com>
2015-02-23 01:53:04 -0500

    and another

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 # location = ENV["REDISCLOUD_URL"] || 'redis://127.0.0.1:6379/0'
 # location = ENV["REDISCLOUD_URL"] || 'redis://192.168.1.2:6379/0'
 # location =  ENV['REDISCLOUD_URL']  || 'redis://redistogo:3f3aed940e56401dee404f708897bf6b@angelfish.redistogo.com:9328/'
-location =   'redis://redistogo:3f3aed940e56401dee404f708897bf6b@angelfish.redistogo.com:9328/'
+location =   'redis://redistogo:3f3aed940e56401dee404f708897bf6b@angelfish.redistogo.com:9328'
 
 # powerful-savannah-9626.herokuapp.com
 uri = URI.parse(location)
